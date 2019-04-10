007aadd6f39a570c0c6c011ab2a1173fa2803ddd
Ilya Sirosh <IlyaSirosh@users.noreply.github.com>
2019-04-10 08:57:35 +0300

    update server.js

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 
+
 //Install express server
 const express = require('express');
 const path = require('path');
@@ -6,12 +7,13 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/<name-of-app>'));
+app.use(express.static(__dirname + '/dist/Planer-client'));
 
-app.get('/*', function(req,res) {
+app.get('*', function(req,res) {
 
-  res.sendFile(path.join(__dirname+'/dist/<name-of-app>/index.html'));
+  res.sendFile(path.join(__dirname+'/dist/Planer-client/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
+app.listen(process.env.PORT || 8080);
 app.listen(process.env.PORT || 8080);
